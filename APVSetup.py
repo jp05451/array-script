4496176802ddc823bcd01778f21a102c041e9a0c
jp05451 <jp05451@yahoo.com.tw>
2026-01-11 00:16:53 +0800

    feat: 新增 HTTP 負載均衡器設置功能，並優化 UDP 和 TCP 設置命令

--- APVSetup.py
+++ APVSetup.py
@@ -21,32 +21,34 @@ class APVSetup:
     def setupUDPLoadBalancer(self,pair_index,dry_run=False):
         if dry_run:
             print(f"Dry run: Setting up UDP Load Balancer for pair index {pair_index}")
-            print(f"slb real udp udp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port}")
+            print(f"slb real udp udp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 3 3 60 none")
+            print(f"slb real enable udp_rs_{pair_index}")
+            
+            print(f'slb virtual udp udp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            print('slb virtual enable udp_slb_vs')
             
             # configure UDP load balancer group
             print('slb group method udp_slb_rs_group rr')
-            
             print(f'slb group member udp_slb_rs_group udp_rs_{pair_index}')
             print('slb group enable udp_slb_rs_group')
             
             # configure UDP virtual server
-            print(f'slb virtual udp udp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
-            print('slb virtual enable udp_slb_vs')
             print('slb policy default udp_slb_vs udp_slb_rs_group')
             
         else:
             # configure UDP real servers
-            self.ssh_apv.execute_command(f"slb real udp udp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port}")
+            self.ssh_apv.execute_command(f"slb real udp udp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 3 3 60 none")
+            self.ssh_apv.execute_command(f"slb real enable udp_rs_{pair_index}")
+            
+            self.ssh_apv.execute_command(f'slb virtual udp udp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            self.ssh_apv.execute_command('slb virtual enable udp_slb_vs')
             
             # configure UDP load balancer group
             self.ssh_apv.execute_command('slb group method udp_slb_rs_group rr')
-            
             self.ssh_apv.execute_command(f'slb group member udp_slb_rs_group udp_rs_{pair_index}')
             self.ssh_apv.execute_command('slb group enable udp_slb_rs_group')
             
             # configure UDP virtual server
-            self.ssh_apv.execute_command(f'slb virtual udp udp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
-            self.ssh_apv.execute_command('slb virtual enable udp_slb_vs')
             self.ssh_apv.execute_command('slb policy default udp_slb_vs udp_slb_rs_group')
         
         
@@ -54,28 +56,60 @@ class APVSetup:
     def setupTCPLoadBalancer(self,pair_index,dry_run=False):
         if dry_run:
             print(f"Dry run: Setting up TCP Load Balancer for pair index {pair_index}")
-            print(f"slb real tcp tcp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port}")
+            print(f"slb real tcp tcp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 none")
+            print(f"slb real enable tcp_rs_{pair_index}")
+            
+            print(f'slb virtual tcp tcp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            print('slb virtual enable tcp_slb_vs')
             
             print('slb group method tcp_slb_rs_group rr')
             print(f'slb group member tcp_slb_rs_group tcp_rs_{pair_index}')
             print('slb group enable tcp_slb_rs_group')
             
-            print(f'slb virtual tcp tcp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
-            print('slb virtual enable tcp_slb_vs')
             print('slb policy default tcp_slb_vs tcp_slb_rs_group')
             
         else:
         # setup TCP real servers  
-            self.ssh_apv.execute_command(f"slb real tcp tcp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port}")
+            self.ssh_apv.execute_command(f"slb real tcp tcp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 none")
+            self.ssh_apv.execute_command(f"slb real enable tcp_rs_{pair_index}")
+            
+            self.ssh_apv.execute_command(f'slb virtual tcp tcp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            self.ssh_apv.execute_command('slb virtual enable tcp_slb_vs')
         
             self.ssh_apv.execute_command('slb group method tcp_slb_rs_group rr')     
             self.ssh_apv.execute_command(f'slb group member tcp_slb_rs_group tcp_rs_{pair_index}')
             self.ssh_apv.execute_command('slb group enable tcp_slb_rs_group')
             
-            self.ssh_apv.execute_command(f'slb virtual tcp tcp_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
-            self.ssh_apv.execute_command('slb virtual enable tcp_slb_vs')
             self.ssh_apv.execute_command('slb policy default tcp_slb_vs tcp_slb_rs_group')
         
+    def setupHTTPLoadBalancer(self,pair_index,dry_run=False):
+        if dry_run:
+            print(f"Dry run: Setting up TCP Load Balancer for pair index {pair_index}")
+            print(f"slb real tcp tcp_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 none")
+            print(f"slb real enable http_rs_{pair_index}")
+            
+            print(f'slb virtual http http_slb_vs_{self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            print('slb virtual enable http_slb_vs')
+            
+            print('slb group method http_slb_rs_group rr')
+            print(f'slb group member http_slb_rs_group http_rs_{pair_index}')
+            print('slb group enable http_slb_rs_group')
+            
+            print('slb policy default http_slb_vs http_slb_rs_group')
+            
+        else:
+        # setup TCP real servers  
+            self.ssh_apv.execute_command(f"slb real http http_rs_{pair_index} {self.pairs[pair_index].server.server_ip} {self.pairs[pair_index].server.listen_port} 0 none")
+            self.ssh_apv.execute_command(f"slb real enable http_rs_{pair_index}")
+            
+            self.ssh_apv.execute_command(f'slb virtual http http_slb_vs {self.pairs[pair_index].client.virtual_server_ip} {self.pairs[pair_index].client.virtual_server_port}')
+            self.ssh_apv.execute_command('slb virtual enable http_slb_vs')
+        
+            self.ssh_apv.execute_command('slb group method http_slb_rs_group rr')     
+            self.ssh_apv.execute_command(f'slb group member http_slb_rs_group http_rs_{pair_index}')
+            self.ssh_apv.execute_command('slb group enable http_slb_rs_group')
+            
+            self.ssh_apv.execute_command('slb policy default http_slb_vs http_slb_rs_group')
         
     
     def setup_apv(self, dry_run=False):
@@ -91,8 +125,13 @@ class APVSetup:
             elif protocol == 'tcp':
                 print('TCP')
                 self.setupTCPLoadBalancer(pair_index=i,dry_run=dry_run)
+            elif protocol == 'http':
+                print('HTTP')
+                self.setupHTTPLoadBalancer(pair_index=i,dry_run=dry_run)
             else:
                 raise ValueError(f"Unsupported protocol: {protocol}")
+        if not dry_run:
+            self.ssh_apv.execute_command('write memory',real_time=True)
     
     def connect(self):
         self.ssh_apv.connect(persistent_session=True,)
@@ -100,10 +139,28 @@ class APVSetup:
     def disconnect(self):
         self.ssh_apv.close()
         
+def argParser():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="APV Load Balancer Setup Script")
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="Run the setup in dry-run mode without making changes",
+    )
+    parser.add_argument(
+        '-c', '--config',
+        type=str,
+        default="config.yaml",
+        help="Path to the configuration YAML file"
+    )
+    return parser.parse_args()
 if __name__ == "__main__":
+    args = argParser()
     config = Config()
-    config.from_yaml("config.yaml")
+    config.from_yaml(args.config)
     apv_setup = APVSetup(config)
     apv_setup.connect()
-    apv_setup.setup_apv(dry_run=True)
+    apv_setup.setup_apv(dry_run=args.dry_run)
     apv_setup.disconnect()
